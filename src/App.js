f557b5c33de7b7f31a7ce7eafc0c56da8ab1882b
Viktoriia Ksenofontova <glory_viktory@hotmail.com>
2021-09-17 14:27:02 +0300

    hero

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Header from './components/Header';
+import Hero from './components/Hero';
 import './App.scss';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Hero />
     </div>
   );
 }
---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,40 @@
+import { ReactComponent as Arrow } from '../../images/downArrow.svg';
+import illustration from '../../images/illustration.png';
+import s from './Hero.module.scss';
+
+const Hero = () => {
+  return (
+    <section className={s.hero}>
+      <div className={s.heroContent}>
+        <h1 class={s.title}>
+          Металлопластиковые <br />
+          окна в Херсоне
+        </h1>
+        <h2 className={s.subtitle}>"Когда цель - качественные окна"</h2>
+        <ul className={s.heroList}>
+          <li className={s.listItem}>
+            <span>15</span>
+            <p>
+              лет <br />
+              на рынке
+            </p>
+          </li>
+          <li className={s.listItem}>
+            <span>8</span>
+            <p>брендов окон</p>
+          </li>
+          <li className={s.listItem}>
+            <span>5</span>
+            <p>
+              лет <br />
+              гарантии
+            </p>
+          </li>
+        </ul>
+        <Arrow className={s.heroBtn} />
+      </div>
+      <img src={illustration} alt="window in focus" />
+    </section>
+  );
+};
+export default Hero;
